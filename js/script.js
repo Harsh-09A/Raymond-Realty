047e7975b47d6102719eb3d40c2f95ce849ce305
Harsh Athavale <harsh.autowebbed@gmail.com>
2024-01-09 17:36:09 +0530

    Added: Location, About & Footer

--- js/script.js
+++ js/script.js
@@ -19,3 +19,20 @@ $(".count").each(function () {
 
 // Fancy Box
 // Fancybox.bind('[data-fancybox="gallery"]', {});
+
+// Toggle Content Button
+function toggleContent(dotsId, moreTextId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreTextId);
+  var btnText = document.getElementById(btnId);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More ...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
